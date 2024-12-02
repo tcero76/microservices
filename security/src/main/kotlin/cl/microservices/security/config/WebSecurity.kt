ebbb083c1ca22bf0c65dbb6c0fd018f57620b13f
Leonardo Lastra <leonardo.lastra@gmail.com>
2024-12-02 14:19:21 -0300

    ordenar

--- security/src/main/kotlin/cl/microservices/security/config/WebSecurity.kt
+++ security/src/main/kotlin/cl/microservices/security/config/WebSecurity.kt
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2Res
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.convert.converter.Converter
-import org.springframework.core.env.Environment
 import org.springframework.http.HttpMethod
-import org.springframework.security.authentication.AbstractAuthenticationToken
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
@@ -18,7 +16,6 @@ import org.springframework.security.oauth2.core.OAuth2TokenValidator
 import org.springframework.security.oauth2.jwt.*
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter
 import org.springframework.security.web.SecurityFilterChain
-import java.time.Duration
 import java.util.stream.Collectors
 
 @Configuration
