f862f2fa19c376774d968ff5fefb67e735542f30
Leonardo Lastra <leonardo.lastra@gmail.com>
2024-12-02 14:18:36 -0300

    reordenar configuracion security

--- security/src/main/kotlin/cl/microservices/security/config/WebSecurity.kt
+++ security/src/main/kotlin/cl/microservices/security/config/WebSecurity.kt
@@ -4,29 +4,31 @@ import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.convert.converter.Converter
 import org.springframework.core.env.Environment
 import org.springframework.http.HttpMethod
+import org.springframework.security.authentication.AbstractAuthenticationToken
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator
 import org.springframework.security.oauth2.core.OAuth2TokenValidator
 import org.springframework.security.oauth2.jwt.*
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter
 import org.springframework.security.web.SecurityFilterChain
 import java.time.Duration
+import java.util.stream.Collectors
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 class WebSecurity(val oAuth2ResourceServerProperties: OAuth2ResourceServerProperties,
-    val environment: Environment
 ) {
     val log = KotlinLogging.logger {  }
     @Bean
     open fun configure(http:HttpSecurity):SecurityFilterChain  {
-        var jwtAuthenticationConverter = JwtAuthenticationConverter()
-        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(AuthServerRoleConverter())
         http.run {
             authorizeHttpRequests {
                 it.requestMatchers(HttpMethod.GET,"/actuator/**").permitAll()
@@ -36,13 +38,25 @@ class WebSecurity(val oAuth2ResourceServerProperties: OAuth2ResourceServerProper
             }
             oauth2ResourceServer {
                 it.jwt {
-                    it.jwtAuthenticationConverter(jwtAuthenticationConverter)
+                    it.jwtAuthenticationConverter(jwtAuthenticationConverter())
                     it.decoder(jwtDecoder())
                 }
             }
         }
         return http.build()
     }
+    fun jwtAuthenticationConverter():JwtAuthenticationConverter {
+        var jwtAuthenticationConverter = JwtAuthenticationConverter()
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(authServerRoleConverter())
+        return jwtAuthenticationConverter
+    }
+    fun authServerRoleConverter(): Converter<Jwt, Collection<GrantedAuthority>> = Converter {
+        val realmAccess = it.claims.get("roles") as Collection<String>
+        (it.claims.get("roles") as ArrayList<String>).stream()
+            .map(::SimpleGrantedAuthority)
+            .collect(Collectors.toList())
+    }
+
     private fun jwtDecoder(): JwtDecoder {
         var issuer:String = oAuth2ResourceServerProperties.getJwt().getIssuerUri()
         log.info { "ISSUER: la url es: ${issuer}" }
