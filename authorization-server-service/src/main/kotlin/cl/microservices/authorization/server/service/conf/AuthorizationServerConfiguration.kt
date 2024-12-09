a8f2755b6fde387e5459a968620f030020907399
Leonardo Lastra <leonardo.lastra@gmail.com>
2024-12-09 17:04:15 -0300

    ordenar SecurityFilterChain en el AuthorizationServer

--- authorization-server-service/src/main/kotlin/cl/microservices/authorization/server/service/conf/AuthorizationServerConfiguration.kt
+++ authorization-server-service/src/main/kotlin/cl/microservices/authorization/server/service/conf/AuthorizationServerConfiguration.kt
@@ -8,10 +8,6 @@ import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.Ordered
-import org.springframework.core.annotation.Order
-import org.springframework.security.config.Customizer
-import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.core.Authentication
 import org.springframework.security.oauth2.core.AuthorizationGrantType
@@ -21,17 +17,11 @@ import org.springframework.security.oauth2.server.authorization.OAuth2TokenType
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository
-import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration
-import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings
 import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext
 import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer
-import org.springframework.security.web.SecurityFilterChain
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher
 import java.security.KeyFactory
-import java.security.KeyPair
-import java.security.KeyPairGenerator
 import java.security.interfaces.RSAPrivateKey
 import java.security.interfaces.RSAPublicKey
 import java.security.spec.PKCS8EncodedKeySpec
@@ -90,15 +80,6 @@ class AuthorizationServerConfiguration {
                 context.claims.expiresAt(Instant.now().plusSeconds(600))
             }
         }
-    }
-    @Bean
-    @Order(Ordered.HIGHEST_PRECEDENCE)
-    fun authorizationServerSecurityFilterChain(http:HttpSecurity): SecurityFilterChain {
-        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http)
-        http.getConfigurer(OAuth2AuthorizationServerConfigurer::class.java).oidc(Customizer.withDefaults())
-        return http
-            .formLogin(Customizer.withDefaults())
-            .build()
     }
 
     @Bean
--- authorization-server-service/src/main/kotlin/cl/microservices/authorization/server/service/conf/SpringSecurityConfiguration.kt
+++ authorization-server-service/src/main/kotlin/cl/microservices/authorization/server/service/conf/SpringSecurityConfiguration.kt
@@ -3,9 +3,13 @@ package cl.microservices.authorization.server.service.conf
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
 import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer
 import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher
@@ -13,15 +17,28 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher
 
 @Configuration
 class SpringSecurityConfiguration {
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    fun authorizationServerSecurityFilterChain(http:HttpSecurity): SecurityFilterChain {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http)
+        http.getConfigurer(OAuth2AuthorizationServerConfigurer::class.java).oidc(Customizer.withDefaults())
+        return http
+            .formLogin(Customizer.withDefaults())
+            .build()
+    }
     @Bean
     fun configureSecurityFilterChain(http:HttpSecurity):SecurityFilterChain {
         http.run {
-            authorizeHttpRequests { it.anyRequest().permitAll() } }
-            .formLogin(Customizer.withDefaults())
-            .logout {
-                it.logoutRequestMatcher(AntPathRequestMatcher("/logout"))
-                    .permitAll()
+            authorizeHttpRequests {
+                it.anyRequest().permitAll()
             }
+        }
+        .formLogin(Customizer.withDefaults())
+        .logout {
+            it.logoutRequestMatcher(AntPathRequestMatcher("/logout"))
+                .permitAll()
+        }
         return http.build()
     }
 }
