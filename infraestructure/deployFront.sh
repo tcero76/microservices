efca439182934b23483bd4e6e9c2fc4228170c53
Leonardo Lastra <leonardo.lastra@gmail.com>
2024-12-10 18:29:20 -0300

    update infra config

--- infraestructure/deployFront.sh
+++ infraestructure/deployFront.sh
@@ -1,1 +1,6 @@
-docker run -it --rm -v $PWD/../frontend:/app -w /app/frontend/ node:22.3.0 npm run build
+#!/bin/bash
+set -o allexport
+source .env
+set +o allexport
+
+docker run -it --rm -v $PWD/../frontend:/app -w /app/frontend/ -e VITE_APP_REDIRECT_URL=http://localhost:${EXTERNAL_PORT} node:22.3.0 npm run build
