ec9617c10c56aaa1d5528711f394927bd456f071
Leonardo Lastra <leonardo.lastra@gmail.com>
2025-01-10 21:54:54 -0300

    Crea deployBuilder

--- infraestructure/deployFront.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -o allexport
-source .env
-set +o allexport
-
-docker run -it --rm -v $PWD/../frontend:/app -w /app/frontend/ -e VITE_APP_REDIRECT_URL=http://localhost:${EXTERNAL_PORT} node:22.3.0 npm run build
