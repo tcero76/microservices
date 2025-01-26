6817ebfa7b263f4321ca7777a355d0472ded5660
Leonardo Lastra <leonardo.lastra@gmail.com>
2025-01-26 10:00:40 -0300

    add EXTERNAL_HOST

--- deployBuilder.sh
+++ deployBuilder.sh
@@ -4,15 +4,16 @@ source infraestructure/.env
 set +o allexport
 
 docker build --build-arg EXTERNAL_PORT=${EXTERNAL_PORT} \
+  --build-arg EXTERNAL_HOST=${EXTERNAL_HOST} \
   -t tcero76/frontend-service:${VERSION} \
   -f infraestructure/Dockerfile/Dockerfile.frontend .
 echo "DONE: Build del servicio: frontend-service"
 
-for servicio in "${SERVICIOS[@]}"; do
-docker build --build-arg JAR_PATH=$servicio \
-  -t tcero76/$servicio:${VERSION} \
-  -f infraestructure/Dockerfile/Dockerfile.service .
-echo "DONE: Build del servicio: $servicio"
-done
+#for servicio in "${SERVICIOS[@]}"; do
+#docker build --build-arg JAR_PATH=$servicio \
+#  -t tcero76/$servicio:${VERSION} \
+#  -f infraestructure/Dockerfile/Dockerfile.service .
+#echo "DONE: Build del servicio: $servicio"
+#done
 
 echo "Build terminado"
